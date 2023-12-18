641c8b0859d50888673caf6d78651579e2a65ee7
Rodrigo <r.barboza9502@gmail.com>
2023-12-18 12:13:30 -0300

    First test with axios

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,15 +2,22 @@ import './styles.css'
 import ButtonInverse from '../../../components/ButtonInverse';
 import ButtonPrimary from '../../../components/ButtonPrimary';
 import ProductDetailsCard from '../../../components/ProductDetailsCard';
-import * as productService from "../../../services/product-service";
-import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../../models/product';
+import axios from 'axios';
 
 export default function ProductDetails(){
 
-    const params = useParams();
+    const [product, setProduct] = useState<ProductDTO>();
 
-    const product = productService.findById(Number(params.productId));
+    useEffect(() => {
+      axios.get("http://localhost:8080/products/4").then(response =>{
+        console.log(response.data);
+        setProduct(response.data);
+      })
+      
+    }, [])
 
     return (
       <main>
