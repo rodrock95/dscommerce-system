4fe0a52b3bd19cd5964060793286454e5123d7f5
Rodrigo <r.barboza9502@gmail.com>
2024-02-01 22:43:07 -0300

    requestBackend update withCredentials

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,13 +1,12 @@
+import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
-import * as authService from "./auth-service"
 
 export function findMe(){
 
-    const headers = {
-        Authorization: "Bearer " + authService.getAccessToken()
+    const config: AxiosRequestConfig = {
+        url: "/users/me",
+        withCredentials: true
     }
 
-    console.log(headers)
-
-    return requestBackend({url: `/users/me`, headers})
+    return requestBackend(config)
 }
