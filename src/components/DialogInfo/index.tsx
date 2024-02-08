259270b19504757d3922247a398cdf322704dd42
Rodrigo <r.barboza9502@gmail.com>
2024-02-08 13:02:42 -0300

    Dialog modal info

---
+++ src/components/DialogInfo/index.tsx
@@ -0,0 +1,20 @@
+import ButtonPrimary from "../ButtonPrimary";
+
+type Props ={
+    message: string;
+    onDialogClose: Function;
+}
+
+export default function DialogInfo({message, onDialogClose} : Props){
+
+    return(
+        <div className="dsc-dialog-background" onClick={() => onDialogClose()}>
+            <div className="dsc-dialog-box" onClick={(event) =>event.stopPropagation()}>
+                <h2>{message}</h2>
+                <div className="dsc-dialog-btn-container" onClick={() => onDialogClose()}>
+                    <ButtonPrimary text="Ok" />
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -6,6 +6,7 @@ import * as productService from "../../../services/product-service"
 import { ProductDTO } from "../../../models/product";
 import SearchBar from "../../../components/SearchBar";
 import ButtonNextPage from "../../../components/ButtonNextPage";
+import DialogInfo from "../../../components/DialogInfo";
 
 type QueryParams ={
     page: number;
@@ -13,6 +14,11 @@ type QueryParams ={
 }
 
 export default function ProductListing(){
+
+    const [dialogInfoData, setDialogInfoData] = useState({
+        visible: false,
+        message: "Operação com sucesso!"
+    })
 
     const [isLastPage, setIsLastPage] = useState(false);
 
@@ -40,7 +46,15 @@ export default function ProductListing(){
 
     function handleNextPageClick(){
         setQueryParam({...queryParams, page:queryParams.page +1})
-    }  
+    } 
+
+    function handleDialogInfoClose(){
+        setDialogInfoData({...dialogInfoData, visible: false})
+    }
+
+    function handleDeleteClick(){
+        setDialogInfoData({...dialogInfoData, visible: true})
+    }
 
     return(
         <main>
@@ -73,7 +87,7 @@ export default function ProductListing(){
                                 <td className="dsc-tb768">R$ {product.price.toFixed(2)}</td>
                                 <td className="dsc-txt-left">{product.name}</td>
                                 <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar"/></td>
-                                <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar"/></td>
+                                <td><img onClick={handleDeleteClick} className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar"/></td>
                             </tr>
                         ))
                     }
@@ -85,6 +99,15 @@ export default function ProductListing(){
                 <ButtonNextPage onNextPage={handleNextPageClick}/>
             }
       </section>
+
+      {
+        dialogInfoData.visible &&
+        <DialogInfo 
+            message={dialogInfoData.message} 
+            onDialogClose={handleDialogInfoClose}
+        />
+      }
+
     </main>
     )
 }
