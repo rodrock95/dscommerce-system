dbbc8cc2035e939c952a0df4831fae5b6f887f38
Rodrigo <r.barboza9502@gmail.com>
2024-02-23 21:56:56 -0300

    Railway deploy BACKEND URL

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,7 +1,7 @@
 export const TOKEN_KEY = "com.devsuperior.dscommerce/Token"
 export const CART_KEY = "com.devsuperior.dscommerce/Cart"
 
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "dscommerce-backend-rodrigo-production.up.railway.app";
 
 export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
 export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
